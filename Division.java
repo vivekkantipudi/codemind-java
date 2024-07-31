e63283c590a929ca9fd2f7ebc0aa90ab2c5f68f5
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2024-07-31 15:11:29 +0530

    import java.util.*; public class d{     public static void main(String[] args)     {         Scanner r=new Scanner(System.in);         int a,b;         a=r.nextInt();         b=r.nextInt();         System.out.println(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class d{
+    public static void main(String[] args)
+    {
+        Scanner r=new Scanner(System.in);
+        int a,b;
+        a=r.nextInt();
+        b=r.nextInt();
+        System.out.println(a/b);
+    }
+}
