803e96dea5c7796c01586915a4e6a416b1b30362
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2024-07-31 15:36:59 +0530

    import java.util.*; public class h{     public static void main(String[] args)     {         Scanner r=new Scanner(System.in);         int a=r.nextInt();         System.out.printf("%d Hour(s) %d Minute(s)",a/60,a%60);     } }

---
+++ HOURS_and_MINUTES.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class h{
+    public static void main(String[] args)
+    {
+        Scanner r=new Scanner(System.in);
+        int a=r.nextInt();
+        System.out.printf("%d Hour(s) %d Minute(s)",a/60,a%60);
+    }
+}
