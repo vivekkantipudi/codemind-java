366eec233721d340be7354e7e382d129c0459007
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2024-07-31 15:43:58 +0530

    import java.util.*; public class r{     public static void main(String[] args)     {         Scanner r=new Scanner(System.in);         int a,b,c;         a=r.nextInt();         b=r.nextInt();         c=r.nextInt();         System.out.println(((a*5)+(b*10))/c);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class r{
+    public static void main(String[] args)
+    {
+        Scanner r=new Scanner(System.in);
+        int a,b,c;
+        a=r.nextInt();
+        b=r.nextInt();
+        c=r.nextInt();
+        System.out.println(((a*5)+(b*10))/c);
+    }
+}
