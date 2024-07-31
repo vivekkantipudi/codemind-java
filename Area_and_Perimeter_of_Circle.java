afcab0c7ea2557d236e9957175c02a62bfd48800
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2024-07-31 15:28:18 +0530

    import java.util.*; public class a{     public static void main(String[] args)     {         Scanner r=new Scanner(System.in);         int a=r.nextInt();         System.out.printf("%.2f %.2f",3.14*a*a,2*3.14*a);
        }
    }

---
+++ Area_and_Perimeter_of_Circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class a{
+    public static void main(String[] args)
+    {
+        Scanner r=new Scanner(System.in);
+        int a=r.nextInt();
+        System.out.printf("%.2f
+%.2f",3.14*a*a,2*3.14*a);
+        
+    }
+}
